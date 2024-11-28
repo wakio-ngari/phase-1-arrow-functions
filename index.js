5934fdda04820dec9377e14eadbc26a1fafa010c
wakio-ngari <ngarijoyce017@gmail.com>
2024-11-28 12:35:18 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(){
+    return 2000/100;
+}
+
+
+const square=(x)=>(x*x);
+     let x=4;
+     console.log(x);
+
+const add=(y,z)=>(y+z);
+    let y=3;
+    let z=4;
+    console.log(y+z);
